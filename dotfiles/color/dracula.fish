89db2ca28f7a5b6d6004db756d61df93c0ebdd0f
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-07-02 19:12:37 +0200

    Add colors. Use selenized.

---
+++ dotfiles/color/dracula.fish
@@ -0,0 +1,51 @@
+# Dracula
+# https://draculatheme.com/
+
+set background 282A36
+set selection 44475A
+set foreground F8F8F2
+set comment 6272A4
+set red FF5555
+set orange FFB86C
+set yellow FAFA8C
+set green 50FA7B
+set purple BD93F9
+set cyan 8BE9FD
+set pink FF79C6
+
+
+set ansi_black 21222C
+set ansi_red $red
+set ansi_green $green
+set ansi_yellow $yellow
+set ansi_blue BD93F9
+set ansi_magenta $pink
+set ansi_cyan $cyan
+set ansi_white $foreground
+set ansi_bright_black $comment
+set ansi_bright_red FF6E6E
+set ansi_bright_green 69FF94
+set ansi_bright_yellow FFFFA5
+set ansi_bright_blue D6ACFF
+set ansi_bright_magenta FF92DF
+set ansi_bright_cyan A4FFFF
+set ansi_bright_white FFFFFF
+
+set bg_dark $background
+set bg_light $selection
+set fg_dark $comment
+set fg_normal $foreground
+set fg_light $ansi_bright_white
+set light_white $ansi_bright_white
+set red $red
+set green $green
+set yellow $yellow
+set blue $ansi_blue
+set magenta $pink
+set cyan $cyan
+set light_red $ansi_bright_red
+set light_green $ansi_bright_green
+set light_yellow $ansi_bright_magenta
+set light_blue $ansi_bright_blue
+set light_magenta $ansi_bright_magenta
+set light_cyan $ansi_bright_cyan
---
+++ dotfiles/color/nord.fish
@@ -0,0 +1,45 @@
+# Nord
+# https://www.nordtheme.com/docs/colors-and-palettes
+
+# Polar night, background
+set nord00 2E3440 # background dark
+set nord01 3B4252 # status bar color, borders
+set nord02 434C5E # current text editor line
+set nord03 4C566A # comments, etc.
+# Snow storm 
+set nord04 D8DEE9 # variables, constants, attributes, fields
+set nord05 E5E9F0 # subtle highlighted text
+set nord06 ECEFF4 # text color
+# Frost 
+set nord07 8FBCBB # stand out UI, classes, types, primitives
+set nord08 88C0D0 # most visual attention, declarations, functions
+set nord09 81A1C1 # secondary UI elements, operators
+set nord10 5E81AC # tertiary UI elements, pragmas, pre-processors
+# Aurora
+set nord11 BF616A # rouge
+set nord12 D08770 # orange
+set nord13 EBCB8B # jaune
+set nord14 A3BE8C # vert
+set nord15 B48EAD # violet
+
+
+#set bg_dark 282C34
+set bg_dark $nord00
+set bg_light $nord02
+set fg_dark $nord03
+#set fg_normal abb2bf
+set fg_normal $nord04
+set fg_light $nord05
+set light_white $nord06
+set red $nord11
+set green $nord14
+set yellow $nord13
+set blue $nord09
+set magenta $nord15
+set cyan $nord08
+set light_red $nord11
+set light_green $nord14
+set light_yellow $nord12
+set light_blue $nord09
+set light_magenta $nord15
+set light_cyan $nord07
