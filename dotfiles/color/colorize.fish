e0ed996dc9f02ec1d6d8ef6edac62b907d7443cf
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-07-02 19:11:30 +0200

    Add fish/color.fish.tmp -- needs to modify color/colorize.fish to not insert '#' in color names.

--- dotfiles/color/colorize.fish
+++ dotfiles/color/colorize.fish
@@ -2,22 +2,22 @@
 
 source $argv[1]
 
-sed -e "s/#bg_dark/#$bg_dark/g; \
-	s/#bg_light/#$bg_light/g; \
-	s/#fg_dark/#$fg_dark/g; \
-	s/#fg_normal/#$fg_normal/g; \
-	s/#fg_light/#$fg_light/g; \
-	s/#red/#$red/g; \
-	s/#green/#$green/g; \
-	s/#yellow/#$yellow/g; \
-	s/#blue/#$blue/g; \
-	s/#magenta/#$magenta/g; \
-	s/#cyan/#$cyan/g; \
-	s/#light_red/#$light_red/g; \
-	s/#light_green/#$light_green/g; \
-	s/#light_yellow/#$light_yellow/g; \
-	s/#light_blue/#$light_blue/g; \
-	s/#light_magenta/#$light_magenta/g; \
-	s/#light_cyan/#$light_cyan/g; \
+sed -e "s/@bg_dark/$bg_dark/g; \
+	s/@bg_light/$bg_light/g; \
+	s/@fg_dark/$fg_dark/g; \
+	s/@fg_normal/$fg_normal/g; \
+	s/@fg_light/$fg_light/g; \
+	s/@red/$red/g; \
+	s/@green/$green/g; \
+	s/@yellow/$yellow/g; \
+	s/@blue/$blue/g; \
+	s/@magenta/$magenta/g; \
+	s/@cyan/$cyan/g; \
+	s/@light_red/$light_red/g; \
+	s/@light_green/$light_green/g; \
+	s/@light_yellow/$light_yellow/g; \
+	s/@light_blue/$light_blue/g; \
+	s/@light_magenta/$light_magenta/g; \
+	s/@light_cyan/$light_cyan/g; \
 	"
 
