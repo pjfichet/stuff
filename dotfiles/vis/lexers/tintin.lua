6cb297ce7bfd812ffcb8c1e93d5a4338d34e031c
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2024-07-01 12:16:44 +0200

    dotfiles/vis: update tintin.lua to new lpeg syntax. Remove troff.lua.

--- dotfiles/vis/lexers/tintin.lua
+++ dotfiles/vis/lexers/tintin.lua
@@ -1,29 +1,15 @@
--- Copyright 2006-2017 Mitchell mitchell.att.foicica.com. See LICENSE.
--- Tintin++ LPeg lexer.
+-- Copyright 2022-2024 Mitchell. See LICENSE.
+-- ? LPeg lexer.
+-- https://orbitalquark.github.io/scintillua/api.html
 
-local l = require('lexer')
-local token, word_match = l.token, l.word_match
-local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local M = {_NAME = 'tintin'}
+local lexer = lexer
+local P, S = lpeg.P, lpeg.S
 
--- Whitespace.
-local ws = token(l.WHITESPACE, l.space^1)
-
--- Comments.
-local comment = token(l.COMMENT, '#nop' * l.nonnewline^0)
-
--- Strings.
---local sq_str = l.delimited_range("'", true, true)
-local dq_str = l.delimited_range('"', true, true)
-
-local string = token(l.STRING, dq_str)
-
--- Numbers.
-local number = token(l.NUMBER, l.float + l.integer)
+local lex = lexer.new(...)
 
 -- Keywords.
-local keyword_name = word_match({
+lex:add_rule('keyword', lexer.token(lexer.KEYWORD, '#' * lexer.word_match{
 	'act', 'action', 'alias', 'all', 'bell', 'break', 'buffer',
 	'case', 'chat', 'class', 'config', 'continue',
 	'cr', 'cursor', 'debug', 'default', 'delay', 'echo',
@@ -36,43 +22,32 @@ local keyword_name = word_match({
 	'scan', 'script', 'send', 'session', 'showme', 'snoop',
 	'speedwalk', 'split', 'ssl', 'sub', 'substitute',
 	'suspend', 'switch', 'system', 'tab', 'textin', 'ticker',
-	'unticker', 'var', 'variable', 'while', 'write', 'zap',
-}, nil, true)
-local keyword = token(l.KEYWORD, P('#') * P(keyword_name))
+	'unticker', 'var', 'variable', 'while', 'write', 'zap'
+}))
 
 -- Identifiers.
---local identifier =token(l.IDENTIFIER,  P('{') * P(l.word) * P('}') )
+lex:add_rule('identifier', lex:tag(lexer.IDENTIFIER, lexer.word))
 
--- Variables.
-local variable = token(l.VARIABLE,
-                       '$' * (S('!#?*@$') + l.digit^1 + l.word +
-                              l.delimited_range('{}', true, true, true)))
+-- Comments.
+lex:add_rule('comment', lex:tag(lexer.COMMENT, lexer.to_eol('#nop')))
 
--- Colors
-local color = token(l.CONSTANT, '<' * P(l.alnum)^-3 * '>')
+-- Numbers.
+lex:add_rule('number', lex:tag(lexer.NUMBER, lexer.number))
 
 -- Operators.
-local operator = token(l.OPERATOR, S('=!<>+-/*^&|~.,:;?()[]{}'))
+lex:add_rule('operator', lex:tag(lexer.OPERATOR, S('+-*/%^=<>,.{}[]()')))
 
-M._rules = {
-  {'whitespace', ws},
-  {'keyword', keyword},
-  --{'identifier', identifier},
-  {'string', string},
-  {'comment', comment},
-  {'number', number},
-  {'variable', variable},
-  {'color', color},
-  {'operator', operator},
-}
+-- Variables
+lex:add_rule('variable', lex:tag(lexer.VARIABLE, '$' * (lexer.number + lexer.word)) )
 
-M._foldsymbols = {
-  _patterns = {'[a-z]+', '[{}]', '#'},
-  [l.KEYWORD] = {
-    ['if'] = 1, fi = -1, case = 1, esac = -1, ['do'] = 1, done = -1
-  },
-  [l.OPERATOR] = {['{'] = 1, ['}'] = -1},
-  [l.COMMENT] = {['#'] = l.fold_line_comments('#')}
-}
+-- Strings.
+local sq_str = lexer.range("'")
+local dq_str = lexer.range('"')
+lex:add_rule('string', lex:tag(lexer.STRING, sq_str + dq_str))
 
-return M
+-- Fold points.
+--lex:add_fold_point(lexer.KEYWORD, 'start', 'end')
+lex:add_fold_point(lexer.OPERATOR, '{', '}')
+
+
+return lex
